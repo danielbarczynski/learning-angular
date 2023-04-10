a5ba12f3b4a9845e10d0315139ad2d158fd38d55
Daniel Barczyński <daniel.barczynski@gearcode.eu>
2023-04-10 18:29:29 +0200

    data from backend (httpClient)

--- src/app/serivces/task.service.ts
+++ src/app/serivces/task.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { TASKS } from 'src/app/mock-tasks';
 import { Task } from 'src/app/Task';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-  constructor() { }
+  private apiUrl = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS); // turns into observable
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl); // coming from the backend
   }
 }
